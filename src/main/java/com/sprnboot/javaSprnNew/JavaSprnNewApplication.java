21f9dfb1e0327207e11d68c14a1e403567c91047
Vivek Pathi <Vivek_Pathi@ad.infosys.com>
2019-01-02 18:07:46 +0530

    asdfd

---
+++ src/main/java/com/sprnboot/javaSprnNew/JavaSprnNewApplication.java
@@ -0,0 +1,14 @@
+package com.sprnboot.javaSprnNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprnNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprnNewApplication.class, args);
+	}
+
+}
+
